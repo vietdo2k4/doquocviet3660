a96433ec9131c90ea3c6f5c571f4deb219bce25d
vietdo2k4 <vietvuive2k4@gmail.com>
2026-03-26 14:39:34 +0700

    Add repo Sringboot

---
+++ src/main/java/com/example/doquocviet3660/Doquocviet3660Application.java
@@ -0,0 +1,13 @@
+package com.example.doquocviet3660;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doquocviet3660Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Doquocviet3660Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doquocviet3660/Doquocviet3660ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doquocviet3660;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Doquocviet3660ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
